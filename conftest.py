a346d101a8f16bcf3dec085d74e66efa531eb46d
Ksenia Vyushkova <ksenia.vyushkova@gmail.com>
2025-12-02 23:46:39 +0400

    Configure base-url, add options to pytest.ini

--- conftest.py
+++ conftest.py
@@ -1,7 +1,17 @@
 import pytest
 from playwright.sync_api import Page
 
+
+def pytest_addoption(parser):
+    parser.addini("--base-url",
+                  help="Base url of the application",
+                  default="https://coffee-cart.app/")
+
 @pytest.fixture
-def new_menu_page(page: Page):
-    page.goto("https://coffee-cart.app/")
+def new_menu_page(request, page: Page):
+    cmd_base_url_option = request.config.getoption("--base-url")
+    if cmd_base_url_option is (None or ""):
+        page.goto(request.config.getini("--base-url"))
+    else:
+        page.goto(cmd_base_url_option)
     return page
