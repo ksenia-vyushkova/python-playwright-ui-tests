53079d9529b82009c3ae34949a80675f58f7daa3
Ksenia Vyushkova <ksenia.vyushkova@gmail.com>
2025-12-04 19:30:49 +0400

    Mark the tests as sanity/regression/failing

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -1,15 +1,21 @@
 import re
+import pytest
 from playwright.sync_api import Page, expect
 from pages.MenuPage import MenuPage
 
+
+@pytest.mark.regression
 def test_url(base_url, new_menu_page: Page):
     """ Check url of the menu page"""
     expect(new_menu_page).to_have_url(base_url)
 
+@pytest.mark.failing
 def test_failing_test(new_menu_page: Page):
     """ Intentionally fail the test"""
     expect(new_menu_page).to_have_url("https://invalid_url.com")
 
+@pytest.mark.sanity
+@pytest.mark.regression
 def test_adding_first_item_to_cart(new_menu_page: Page):
     """ Check that the first cup can be added to the cart by a simple click"""
     cup_number = 0
@@ -17,6 +23,7 @@ def test_adding_first_item_to_cart(new_menu_page: Page):
     menu_page.click_on_nth_cup(cup_number)
     expect(menu_page.cart_link).to_contain_text(f"({cup_number + 1})")
 
+@pytest.mark.regression
 def test_hover_state(new_menu_page: Page):
     """ Check that a cup is rotated on hover"""
     cup_number = 0
@@ -26,11 +33,3 @@ def test_hover_state(new_menu_page: Page):
     menu_page.hover_over_nth_cup(cup_number)
     expect(cup).to_have_css("cursor", "pointer")
     expect(cup).to_have_css("transform", re.compile("^matrix*"))
-
-
-
-
-
-
-
-
