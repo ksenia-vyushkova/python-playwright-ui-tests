26c3c1ebd2aed1941466e16e60e5df012bb750ba
Ksenia Vyushkova <ksenia.vyushkova@gmail.com>
2025-12-13 22:37:00 +0400

    Add test_adding_and_removing_coffee_from_cart_preview, add more assertions to existing tests

--- tests/test_actions_from_menu_page.py
+++ tests/test_actions_from_menu_page.py
@@ -15,10 +15,13 @@ coffee_item_count = len([coffee_item for
 def test_adding_one_coffee_to_cart(cup_number, new_menu_page: Page):
     """Check that a cup can be added to the cart by a left mouse click."""
     menu_page = MenuPage(new_menu_page)
+    coffee_name = menu_page.get_nth_coffee_item_name(cup_number)
     coffee_price = menu_page.get_nth_coffee_item_price(cup_number)
     menu_page.click_on_nth_cup(cup_number)
     expect(menu_page.cart_link).to_contain_text("cart (1)")
     expect(menu_page.total_value).to_have_text(f"Total: ${coffee_price:.2f}")
+    menu_page.pay_container.hover()
+    expect(menu_page.cart_preview).to_contain_text(f"{coffee_name} x 1")
 
 
 @pytest.mark.regression
@@ -34,6 +37,8 @@ def test_adding_one_coffee_to_cart_with_right_click(new_menu_page: Page):
     expect(menu_page.add_coffee_to_cart_question).not_to_be_visible()
     expect(menu_page.cart_link).to_contain_text("cart (1)")
     expect(menu_page.total_value).to_have_text(f"Total: ${coffee_price:.2f}")
+    menu_page.pay_container.hover()
+    expect(menu_page.cart_preview).to_contain_text(f"{coffee_name} x 1")
 
 
 @pytest.mark.regression
@@ -48,6 +53,8 @@ def test_refusing_to_add_coffee_to_cart(new_menu_page: Page):
     expect(menu_page.add_coffee_to_cart_question).not_to_be_visible()
     expect(menu_page.cart_link).to_contain_text("cart (0)")
     expect(menu_page.total_value).to_have_text("Total: $0.00")
+    menu_page.pay_container.hover()
+    expect(menu_page.cart_preview).not_to_be_visible()
 
 
 @pytest.mark.sanity
@@ -57,12 +64,17 @@ def test_adding_two_coffees_to_cart(new_menu_page: Page):
     first_cup = 3
     second_cup = 7
     menu_page = MenuPage(new_menu_page)
+    first_coffee_name = menu_page.get_nth_coffee_item_name(first_cup)
+    second_coffee_name = menu_page.get_nth_coffee_item_name(second_cup)
     first_coffee_price = menu_page.get_nth_coffee_item_price(first_cup)
     second_coffee_price = menu_page.get_nth_coffee_item_price(second_cup)
     menu_page.click_on_nth_cup(first_cup)
     menu_page.click_on_nth_cup(second_cup)
     expect(menu_page.cart_link).to_contain_text("cart (2)")
     expect(menu_page.total_value).to_have_text(f"Total: ${first_coffee_price + second_coffee_price:.2f}")
+    menu_page.pay_container.hover()
+    expect(menu_page.cart_preview).to_contain_text(f"{first_coffee_name} x 1")
+    expect(menu_page.cart_preview).to_contain_text(f"{second_coffee_name} x 1")
 
 
 @pytest.mark.regression
@@ -91,7 +103,7 @@ def test_adding_coffee_from_cart_preview(new_menu_page: Page):
 @pytest.mark.regression
 def test_removing_coffee_from_cart_preview(new_menu_page: Page):
     """Check that coffee can be correctly removed from the cart preview."""
-    cup_number = 4
+    cup_number = 8
     menu_page = MenuPage(new_menu_page)
     coffee_name = menu_page.get_nth_coffee_item_name(cup_number)
     coffee_price = menu_page.get_nth_coffee_item_price(cup_number)
@@ -108,3 +120,41 @@ def test_removing_coffee_from_cart_preview(new_menu_page: Page):
     expect(menu_page.cart_link).to_contain_text("cart (0)")
     expect(menu_page.total_value).to_have_text("Total: $0.00")
     expect(menu_page.cart_preview).not_to_be_visible()
+
+
+@pytest.mark.regression
+def test_adding_and_removing_coffee_from_cart_preview(new_menu_page: Page):
+    """Combine checks for adding and removing coffee from cart preview."""
+    first_cup_number, second_cup_number, third_cup_number = 3, 5, 1
+    menu_page = MenuPage(new_menu_page)
+    first_coffee_name = menu_page.get_nth_coffee_item_name(first_cup_number)
+    second_coffee_name = menu_page.get_nth_coffee_item_name(second_cup_number)
+    third_coffee_name = menu_page.get_nth_coffee_item_name(third_cup_number)
+    first_coffee_price = menu_page.get_nth_coffee_item_price(first_cup_number)
+    second_coffee_price = menu_page.get_nth_coffee_item_price(second_cup_number)
+    third_coffee_price = menu_page.get_nth_coffee_item_price(third_cup_number)
+    total = first_coffee_price + second_coffee_price + third_coffee_price
+
+    # First, add three coffee items to the cart and check expected state.
+    menu_page.click_on_nth_cup(first_cup_number)
+    menu_page.click_on_nth_cup(second_cup_number)
+    menu_page.click_on_nth_cup(third_cup_number)
+    expect(menu_page.cart_link).to_contain_text("cart (3)")
+    expect(menu_page.total_value).to_have_text(f"Total: ${total:.2f}")
+    menu_page.pay_container.hover()
+    expect(menu_page.cart_preview).to_contain_text(f"{first_coffee_name} x 1")
+    expect(menu_page.cart_preview).to_contain_text(f"{second_coffee_name} x 1")
+    expect(menu_page.cart_preview).to_contain_text(f"{third_coffee_name} x 1")
+
+    # Add one coffee item and remove another coffee item from the cart preview.
+    menu_page.add_from_cart_preview_by_name(first_coffee_name)
+    menu_page.add_from_cart_preview_by_name(second_coffee_name)
+    menu_page.add_from_cart_preview_by_name(second_coffee_name)
+    menu_page.remove_from_cart_preview_by_name(third_coffee_name)
+    total = 2 * first_coffee_price + 3 * second_coffee_price
+
+    expect(menu_page.cart_link).to_contain_text("cart (5)")
+    expect(menu_page.total_value).to_have_text(f"Total: ${total:.2f}")
+    expect(menu_page.cart_preview).to_contain_text(f"{first_coffee_name} x 2")
+    expect(menu_page.cart_preview).to_contain_text(f"{second_coffee_name} x 3")
+    expect(menu_page.cart_preview).not_to_contain_text(f"{third_coffee_name} x")
