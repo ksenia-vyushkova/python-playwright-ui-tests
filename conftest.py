d6b458d47f6a93f34f048d0dfcc9330109ccbfd8
Ksenia Vyushkova <ksenia.vyushkova@gmail.com>
2025-12-15 19:54:08 +0400

    Change handling of base url

--- conftest.py
+++ conftest.py
@@ -2,17 +2,8 @@ import pytest
 from playwright.sync_api import Page
 
 
-def pytest_addoption(parser):
-    parser.addini("--base-url",
-                  help="Base url of the application",
-                  default="https://coffee-cart.app/")
-
-
-def base_url(config):
-    return config.getoption("--base-url") or config.getini("--base-url")
-
-
 @pytest.fixture
-def new_menu_page(request, page: Page):
-    page.goto(base_url(request.config))
+def new_menu_page(page: Page):
+    """ Navigate to base url."""
+    page.goto("/")
     yield page
